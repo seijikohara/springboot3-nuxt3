7b016ca5bd2ebafc09c17eb05fccd2d8c8928c42
Seiji Kohara <sage.0118@gmail.com>
2023-03-18 23:18:48 +0900

    Fix jar file name generated by bootJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "3.0.4"
@@ -46,4 +47,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<BootJar> {
+    archiveFileName.set("app.jar")
 }
