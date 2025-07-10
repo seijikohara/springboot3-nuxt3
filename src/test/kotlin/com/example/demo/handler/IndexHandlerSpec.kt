1422e0d700fa848f00bfc92e810c7ef158e911f7
Seiji Kohara <sage.0118@gmail.com>
2025-07-10 11:39:07 +0900

    feat: add Kotest tests for application context and handlers

---
+++ src/test/kotlin/com/example/demo/handler/IndexHandlerSpec.kt
@@ -0,0 +1,48 @@
+package com.example.demo.handler
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerRequest
+
+@SpringBootTest
+class IndexHandlerSpec(
+    private val indexHandler: IndexHandler
+) : FunSpec() {
+    init {
+        test("getIndex should return index file content with OK status") {
+            val mockRequest = mockk<ServerRequest>()
+            
+            runBlocking {
+                val response = indexHandler.getIndex(mockRequest)
+
+                response.statusCode().value() shouldBe 200
+                response.headers().contentType shouldBe MediaType.TEXT_HTML
+            }
+        }
+
+        test("getIndex should set TEXT_HTML content type") {
+            val mockRequest = mockk<ServerRequest>()
+            
+            runBlocking {
+                val response = indexHandler.getIndex(mockRequest)
+                
+                response.headers().contentType shouldBe MediaType.TEXT_HTML
+            }
+        }
+
+        test("getIndex should return application properties index file") {
+            val mockRequest = mockk<ServerRequest>()
+            
+            runBlocking {
+                val response = indexHandler.getIndex(mockRequest)
+                
+                response.statusCode().value() shouldBe 200
+            }
+        }
+    }
+}
